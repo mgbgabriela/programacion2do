d30740b8d690efdb5cc5ec2e7db472c326a68703
miguelgabriela <maria.barrera@municipiodetandil.edu.ar>
2023-06-18 00:06:21 -0300

    Carga pistas estaticas

--- pistaAudio/JS/scripts.js
+++ pistaAudio/JS/scripts.js
@@ -1,65 +1,64 @@
 let btnAgregar = document.getElementById("btnAgregar");
-btnAgregar.addEventListener("click", agregar);
+btnAgregar.addEventListener("click",agregar);
 
 let btnDuracion = document.querySelector("#btnDuracion");
 btnDuracion.addEventListener("click", duracion);
 
 let pistas = [];
 
-function agregar (){
-    let identificador =  parseInt (document.querySelector('#Identificador').value);
-    let titulo = document.getElementById('Titulo').value;
-    let duracion = parseInt(document.getElementById('Duracion').value);
-    let interprete = document.getElementById('Interprete').value;
+    function agregar (){
+        let identificador = parseInt(document.querySelector("#identificador").value);
+        let titulo = document.getElementById("titulo").value;
+        let duracion = parseInt(document.getElementById("duracion").value);
+        let interprete = document.getElementById("interprete").value;
 
 
-    let renglon = {
-        "Identificador": identificador,
-        "Titulo" :titulo,
-        "Duracion": duracion,
-        "Interprete" : interprete
+        let renglon ={
+            "identificador": identificador,
+            "titulo": titulo,
+            "duracion": duracion,
+            "interprete": interprete
+        }
+
+        pistas.push(renglon);
+
+        mostrarPistas();
     }
 
-    pistas.push(renglon);
+    function mostrarPistas(){
+        let html ="";
 
-    mostrarPistas();
-}
-
-function mostrarPistas(){
-    let html = "";
-
-    for (let rg of pistas){
-        html +=`
-        <tr>
-            <td>${rg.identificador}</td>
-            <td>${rg.titulo}</td>
-            <td>${rg.duracion}</td>
-            <td>${rg.interprete}</td>
-        </tr>
-        `;
+        for (let rg of pistas){
+            html +=`
+            <tr>
+                <td>${rg.identificador}</td>
+                <td>${rg.titulo}</td>
+                <td>${rg.duracion}</td>
+                <td>${rg.interprete}</td>
+            </tr>
+            `;
+        }
+            document.querySelector("#tbPistas").innerHTML=html;
     }
-        document.querySelector("#tblPistas").innerHTML= html;
 
+
+    function duracion(){
+console.log("Funcion Duracion")
+        let total = 0;
+        for(let i= 0; i < pistas.length; i++){
+        total += pistas[i].duracion;
+    }
+        let max = pistas[0].duracion;
+        for (let r of pistas){ 
+           if (max < r.duracion)
+           max=r.duracion;
+           
+        } 
+        
     
-}
-
-function duracion (){
-let total = 0;
-let max = {
-    "titulo": " ",
-    "duración": 0
-};
-
-for (let i =0;  i < pistas.lenght; i++){
-    total += pistas[i].duracion;
-    if (pistas[i].duracion>max.Duracion){
-        max.duracion = pistas[i].duracion;
-        max.titulo = pistas [i].titulo;
-    }
-}
-document.querySelector("#pistas").innerHTML = `
-<p>Duracion total: ${total} </p>
-<p>Titulo de la pista más larga es : ${max.titulo} con una duración de : ${max.duración}</p>
-
-`
+    document.querySelector("#total").innerHTML = `
+    <p> Duracion total: ${total} </p>
+    <p> La pista más larga tiene una duracion de: ${max} </p>
+    `;
+    
 }
